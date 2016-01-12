115a136a66619f2fcd59dd7ed2c23240cad37d48
Hugh A. Miles II <hmiles23@ufl.edu>
2016-01-12 18:12:30 -0500

    Added starter project

---
+++ Instagram_HAM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram_HAM
+//
+//  Created by Hugh A. Miles II on 1/12/16.
+//  Copyright © 2016 Hugh A. Miles II. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
