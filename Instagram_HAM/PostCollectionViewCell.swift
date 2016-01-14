e3879783afa06d4504bde1fbb15a6ea18a7b26f6
Hugh A. Miles II <hmiles23@ufl.edu>
2016-01-14 18:22:49 -0500

    updated gitignore

---
+++ Instagram_HAM/PostCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PostCollectionViewCell.swift
+//  Instagram_HAM
+//
+//  Created by Hugh A. Miles II on 1/14/16.
+//  Copyright © 2016 Hugh A. Miles II. All rights reserved.
+//
+
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+    
+}
